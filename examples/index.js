fa404f3202f99a342445693c41b6a6691368b767
wangzhengmin <508795194@qq.com>
2022-05-29 10:21:04 +0800

    init

---
+++ examples/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('app'));
+root.render(
+  <div>hello word</div>
+);
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./examples/index.js",
+  output: {
+    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname,"./dist")
+  },
+  // devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react","@babel/preset-env"]
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname,"./examples/index.html")
+    })
+  ]
+}
